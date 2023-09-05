ca9246ba9ab8e2fad6907c77aabc69bfcf96642d
svegapatta <sureshvgknair@gmail.com>
2023-09-06 02:16:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [changedValue, setChangedValue] = useState();
+  const [flag, setFlag] = useState(true);
+  const [values, setValues] = useState({ input: '', inputType: 'meter', result: '', resultType: 'centimeter' });
+
+  const getHandler = (name) => {
+    return (event) => {
+      setChangedValue(name);
+      setFlag(!flag);
+      setValues({ ...values, [name]: event.target.value });
+    };
+  };
+
+  /**
+   * 
+   * @param {*} value 
+   * @param {*} type meter / kilometer / centimeter
+   * @returns Millimeter value
+   */
+  const convertToMillimeter = (value, type) => {
+    let milli;
+    if (type === 'meter') {
+      milli = value * 1000;
+    } else if (type === 'kilometer') {
+      milli = value * 1000000;
+    } else if (type === 'centimeter') {
+      milli = value * 10;
+    }
+    return milli;
+  };
+
+  /**
+   * 
+   * @param {*} milli 
+   * @param {*} type meter / kilometer / centimeter
+   * @returns converted type value
+   */
+  const convertFromMillimeter = (milli, type) => {
+    let value;
+    if (type === 'meter') {
+      value = milli / 1000;
+    } else if (type === 'kilometer') {
+      value = milli / 1000000;
+    } else if (type === 'centimeter') {
+      value = milli / 10;
+    }
+    return value;
+  };
+
+  useEffect(() => {
+    if (changedValue === 'input' || changedValue === 'inputType') {
+      // convert input into millimeter
+      let input_Milli = convertToMillimeter(values.input, values.inputType);
+      // convert into result type
+      let _result = convertFromMillimeter(input_Milli, values.resultType);
+      setValues({ ...values, result: _result });
+    } else if (changedValue === 'result' || changedValue === 'resultType') {
+      // convert input into millimeter
+      let result_Milli = convertToMillimeter(values.result, values.resultType);
+      let _input = convertFromMillimeter(result_Milli, values.inputType);
+      setValues({ ...values, input: _input });
+    }
+  }, [flag])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h2>Unit Converter</h2>
+        <input type='text' value={values.input} onChange={getHandler('input')} />
+        <span>=</span>
+        <input type='text' value={values.result} onChange={getHandler('result')} /><br />
+        <div className='typescontainer'>
+          <select value={values.inputType} onChange={getHandler('inputType')} >
+            <option value="meter" >Meter</option>
+            <option value="kilometer" >Kilometer</option>
+            <option value="centimeter" >Centimeter</option>
+          </select>
+          <select value={values.resultType} className='resultType' onChange={getHandler('resultType')} >
+            <option value="centimeter" >Centimeter</option>
+            <option value="meter" >Meter</option>
+            <option value="kilometer" >Kilometer</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
